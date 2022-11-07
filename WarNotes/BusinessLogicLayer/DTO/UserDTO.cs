e174f4ce106872db92f28b0c981b4c761c550040
Natalia Herus <natalia.gerus892@gmail.com>
2022-11-07 02:52:39 +0200

    Added enum for roles

--- WarNotes/BusinessLogicLayer/DTO/UserDTO.cs
+++ WarNotes/BusinessLogicLayer/DTO/UserDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLogicLayer.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,5 +13,6 @@ namespace BusinessLogicLayer.Services.DTO
         public string? LastName { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
+        public Role Role { get; set; }
     }
 }
