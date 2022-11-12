4703cece5da2d25085712dea16d3b3aff8393a9a
Taras Vynar <taraspena21@gmail.com>
2022-11-12 21:04:34 +0200

    fixed UI styles

--- WarNotes/WarNotes/View/MainView.xaml.cs
+++ WarNotes/WarNotes/View/MainView.xaml.cs
@@ -33,30 +33,26 @@ namespace WarNotes.View
         {
             for (int i = 0; i < 4; i++)
             {
-                RadioButton rb = new RadioButton() { Content = "Radio button " + i, IsChecked = i == 0 };
-                rb.Checked += (sender, args) => {
-                    Console.WriteLine("Pressed " + (sender as RadioButton).Tag);
-                };
-                rb.Unchecked += (sender, args) => { /* Do stuff */ };
+                RadioButton rb = new RadioButton() { Content = "Category " + i };
+
                 rb.Tag = i;
 
-                categories.Children.Add(rb);
+                rb.Style = (Style)FindResource("categoryButton");
 
-                
+                categories.Children.Add(rb);
             }
         }
         private void LoadHeaders()
         {
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < 9; i++)
             {
-                Button rb = new Button() { Content = "Radio button " + i};
-                rb.Click += LoadArticles; 
-                
+                RadioButton rb = new RadioButton() { Content = "Header " + i };
+
                 rb.Tag = i;
 
+                rb.Style = (Style)FindResource("headerButton");
+
                 headers.Children.Add(rb);
-
-
             }
         }
         private void LoadArticles(object sender, RoutedEventArgs e)
